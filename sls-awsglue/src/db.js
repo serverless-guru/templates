6bee2a8d8ae7f37a044c228a718066acf9de6aba
Gary Jennings <garysjennings@gmail.com>
2020-07-23 13:25:02 -0600

    added glue pattern

---
+++ sls-awsglue/src/db.js
@@ -0,0 +1,69 @@
+module.exports = {
+  seed: (connection) => {
+    return new Promise((res, rej) => {
+      const query = 'CREATE TABLE trees (Id BIGINT, Girth FLOAT, Height BIGINT, Volume FLOAT)';
+      connection.query(query, (err, results, fields) => {
+        if (err) {
+          rej(err)
+        } else {
+          res('success')
+        }
+      })
+    })
+  },
+
+  insert: (connection, input) => {
+    return new Promise((res, rej) => {
+      const query = 'INSERT INTO users values(?, ?)';
+      connection.query(query, [input.id, input.name], (err, results, fields) => {
+        if (err) {
+          rej(err)
+        } else {
+          res(input)
+        }
+      })
+    })
+  },
+
+  getAllUsers: (connection) => {
+    return new Promise((res, rej) => {
+      const query = 'SELECT * FROM students';
+      connection.query(query, (err, results, fields) => {
+        if (err) {
+          rej(err)
+        } else {
+          res(results)
+        }
+      })
+    })
+  },
+
+  selectOneUser: (connection, id) => {
+    return new Promise((res, rej) => {
+      const query = `SELECT id, name FROM users WHERE id=${id}`;
+      connection.query(query, (err, results, fields) => {
+        if (err) {
+          rej(err)
+        } else {
+          res(results[0])
+        }
+      })
+    })
+  },
+
+  deleteUser: (connection, id) => {
+    return new Promise((res, rej) => {
+      const query = `DELETE FROM users WHERE id=${id}`;
+      connection.query(query, (err, results, fields) => {
+        if (err) {
+          rej(err)
+        } else {
+          res(results)
+        }
+      })
+    })
+  }
+}
+
+
+
