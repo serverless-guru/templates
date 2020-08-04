fd6c745e5cbe9a62f0843b7f2db314ea3ba4f4f8
Bril Lopez <hellomynameisbril@gmail.com>
2020-08-03 17:13:34 -0700

    [DBAAS-2157] AWS CloudTrail template to create a CloudTrail stack to log, continuously monitor, and retain account activity related to actions across your AWS infrastructure. Apply the trail across the complete flow of cardholder data within aws infrastructure to meet PCI compliance standards. This template creates an S3 bucket to store log files and a strict IAM policy to access the bucket.

---
+++ sls-cloudtrail-pci-compliance/webpack.config.js
@@ -0,0 +1,11 @@
+const slsw = require('serverless-webpack');
+module.exports = {
+  target: 'node',
+  entry: slsw.lib.entries,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  node: false,
+  optimization: {
+    minimize: false,
+  },
+  devtool: 'inline-cheap-module-source-map',
+};
