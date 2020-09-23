54e92cd7712bab66a1603b5061c9fb7f58f6c543
Almir Zulic <almir@bind.ba>
2020-09-23 03:33:36 +0200

    angular/vue/react setup cidcd sls

---
+++ sls-fullstack-with-cicd-on-frontend/README.md
@@ -0,0 +1,29 @@
+# Serverless full stack with CI/CD for frontend app (Angular/React/Vue)
+
+## Prerequisites
+  - serverless pro account
+  - aws profile 
+  - CodeCommit credentials generated inside AWS IAM 
+
+## Summary
+Following stack will create:
+  - Test and Production S3 buckets with website hosting
+  - CodeCommit repository 
+  - CodeBuild projects with buildspec for master and development branch
+  - CodePipelines for test and production
+  - Hello world lambda function
+
+Fronted application will be deploy on test, as well as on production bucket. Whenever you push changes to master branch Pipeline will build and deploy your app to the production bucket. Push to development branch will build and deploy app to the test bucket. 
+
+## Instructions
+
+1. Loging to your serverless [dashboard](https://app.serverless.com) then create app.
+
+2. Create the following parameters:
+    - repositoryName
+    - testBucket
+    - productionBucket
+
+3. Update serverless.yml file app and org properties
+
+4. run `./deploy.sh YOUR_REGION YOUR_AWS_PROFILE react|vue|angular` from your terminal
---
+++ sls-fullstack-with-cicd-on-frontend/deploy.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+region=$1
+profile=$2
+type=$3 # angular, vue, react
+repository=$(sls param get --name repositoryName)
+testBucket=$(sls param get --name testBucket)
+productionBucket=$(sls param get --name productionBucket)
+dir=$(pwd)
+
+# deploy infra
+sls deploy --stage prod --region $region --aws-profile $profile 
+
+# CodeCommit credential helper - you must have GitCredentials
+# https://docs.aws.amazon.com/codecommit/latest/userguide/setting-up-gc.html
+git config --global credential.helper '!aws --profile '"$profile"' codecommit credential-helper $@'
+
+echo "*** Create frontend app ***"
+if [[ "$type" == "angular" ]]; then
+
+  echo "*** Angular detected ***"
+
+  # install angular cli
+  # npm i -g @angular/cli
+
+  # generate angular app
+
+  cd /
+  ng new frontend --directory="$dir/../frontend" --defaults=true
+  cd $dir
+  # prepare production buildspec file
+  sed s%\${BUCKET}%$productionBucket%g ./buildspecs/angular/buildspec.yml > $dir/../frontend/buildspec.yml 
+
+  cd $dir/../frontend/
+
+  git remote add origin https://git-codecommit.$region.amazonaws.com/v1/repos/$repository
+
+  # push to master 
+  git add .
+  git commit -m "production buildspec setup"
+  git push origin master
+
+  # prepare test buildspec file
+  git checkout -b development
+  sed s%\${BUCKET}%$testBucket%g ./buildspecs/angular/buildspec.yml > $dir/../frontend/devbuildspec.yml 
+  git add .
+  git commit -m "test buildspec setup"
+  git push -u origin development
+
+elif [[ "$type" == "react"* ]]; then
+
+  echo "*** React detected ***"
+
+  cd ..
+  npx create-react-app frontend
+
+  cd $dir
+  # prepare production buildspec file
+  sed s%\${BUCKET}%$productionBucket%g ./buildspecs/react/buildspec.yml > $dir/../frontend/buildspec.yml 
+  cd $dir/../frontend/
+  git remote add origin https://git-codecommit.$region.amazonaws.com/v1/repos/$repository
+
+  # push to master 
+  git add .
+  git commit -m "production buildspec setup"
+  git push origin master
+
+  # prepare test buildspec file
+  git checkout -b development
+
+  cd $dir
+  sed s%\${BUCKET}%$testBucket%g ./buildspecs/react/buildspec.yml > $dir/../frontend/devbuildspec.yml 
+  cd $dir/../frontend/
+  
+  git add .
+  git commit -m "test buildspec setup"
+  git push -u origin development
+
+elif [[ "$type" == "vue"* ]]; then
+
+  echo "*** Vue detected ***"
+  npm install -g @vue/cli @vue/cli-service-global
+  cd ..
+  vue create frontend --default
+
+  # prepare production buildspec file
+  cd $dir
+  sed s%\${BUCKET}%$productionBucket%g ./buildspecs/vue/buildspec.yml > $dir/../frontend/buildspec.yml 
+  cd $dir/../frontend/
+
+  git remote add origin https://git-codecommit.$region.amazonaws.com/v1/repos/$repository
+
+  # push to master 
+  git add .
+  git commit -m "production buildspec setup"
+  git push origin master
+
+  # prepare test buildspec file
+  git checkout -b development
+
+  cd $dir
+  sed s%\${BUCKET}%$testBucket%g ./buildspecs/vue/buildspec.yml > $dir/../frontend/devbuildspec.yml 
+  cd $dir/../frontend/
+
+  git add .
+  git commit -m "test buildspec setup"
+  git push -u origin development
+
+fi
+
