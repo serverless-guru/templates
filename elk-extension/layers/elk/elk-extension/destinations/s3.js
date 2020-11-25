c95bec038fa24e12c6131ec7c33ecaf37757f294
edujugon <edujugon@gmail.com>
2020-11-25 23:52:12 +0100

    Code refactoring and destination improvements

--- elk-extension/layers/elk/elk-extension/destinations/s3.js
+++ elk-extension/layers/elk/elk-extension/destinations/s3.js
@@ -6,21 +6,21 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-const S3_BUCKET_NAME = process.env.LOGS_DESTINATION_BUCKET_NAME;
-const S3_OBJECT_NAME = process.env.LOGS_DESTINATION_OBJECT_NAME;
+const S3_BUCKET_NAME = process.env.EXTENSION_LOG_DESTINATION_S3_BUCKET_NAME;
+const S3_OBJECT_NAME = process.env.EXTENSION_LOG_DESTINATION_S3_OBJECT_NAME;
 
-const uploadLogsToS3 = (data) => {
+const sendLogs = (logs) => {
     const fileName = S3_OBJECT_NAME + '-' + new Date().toISOString() + '.log'
 
     const params = {
         Bucket: S3_BUCKET_NAME,
         Key: fileName,
-        Body: data
+        Body: logs
     };
 
     return s3.upload(params).promise();
 }
 
 module.exports = {
-  uploadLogsToS3
+    sendLogs
 };
--- elk-extension/layers/elk/elk-extension/index.js
+++ elk-extension/layers/elk/elk-extension/index.js
@@ -2,13 +2,13 @@
 const { register, next } = require('./extensions-api');
 const { subscribeToLogsAPI, MAX_ITEMS, MAX_BYTES, TIMEOUT_MS } = require('./logs-api');
 const { createHTTPServer } = require('./http-server');
-const { uploadLogsToS3 } = require('./destinations/s3');
+const registeredDestinations = require('./destinations/index').registeredDestinations();
 
 const AWS = require('aws-sdk');
 
-const LOGS_MAX_ITEMS = process.env.LOGS_MAX_ITEMS || MAX_ITEMS;
-const LOGS_MAX_BYTES = process.env.LOGS_MAX_BYTES || MAX_BYTES;
-const LOGS_TIMEOUT_MS = process.env.LOGS_TIMEOUT_MS || TIMEOUT_MS;
+const EXTENSION_LOG_MAX_ITEMS = process.env.EXTENSION_LOG_MAX_ITEMS || MAX_ITEMS;
+const EXTENSION_LOG_MAX_BYTES = process.env.EXTENSION_LOG_MAX_BYTES || MAX_BYTES;
+const EXTENSION_LOG_TIMEOUT_MS = process.env.EXTENSION_LOG_TIMEOUT_MS || TIMEOUT_MS;
 
 /** Store logs from Lambda Logs API */
 const logs = [];
@@ -46,9 +46,9 @@ const handleInvoke = async (event) => {
         {
             extensionId,
             subscribeTo: ['function'],
-            maxItems: Number(LOGS_MAX_ITEMS),
-            maxBytes: Number(LOGS_MAX_BYTES),
-            timeoutMs: Number(LOGS_TIMEOUT_MS)
+            maxItems: Number(EXTENSION_LOG_MAX_ITEMS),
+            maxBytes: Number(EXTENSION_LOG_MAX_BYTES),
+            timeoutMs: Number(EXTENSION_LOG_TIMEOUT_MS)
         }
     );
     console.log('Logs API subscription', { logsAPISubscription });
@@ -61,9 +61,14 @@ const handleInvoke = async (event) => {
 
         while (logs.length > 0) {
             const logBatch = logs.shift();
-
-            await uploadLogsToS3(logBatch);
+            // await uploadLogsToS3(logBatch);
+            await Promise.all(
+                registeredDestinations.map(async ({ sendLogs }) => {
+                    await sendLogs(logBatch);
+                })
+            )
         }
+        // Uncomment to add logic on INVOKE and SHUTDOWN events.
         // switch (event.eventType) {
         //     case EventType.SHUTDOWN:
         //         await handleShutdown(event);
