62027b307f72a8e877967396063956f68368a888
edujugon <edujugon@gmail.com>
2020-12-01 13:00:22 +0100

    MSK improvements

--- kafka-msk/resources/lambda/functions/msk-producer/index.js
+++ kafka-msk/resources/lambda/functions/msk-producer/index.js
@@ -1,25 +1,28 @@
 const { createKafkaProducer, sendMessageToMSK } = require('./msk-producer');
 
-const APP_NAME = process.env.APP_NAME;
-const MY_TOPIC = process.env.MY_TOPIC;
+const ADH_TOPIC = process.env.ADH_TOPIC;
 const KAFKA_HOST = process.env.KAFKA_HOST;
 
 let kafkaProducer = null;
-module.exports.handler = async (event, context) => {
 
-  if (!kafkaProducer) {
-    kafkaProducer = await createKafkaProducer({
-      clientId: APP_NAME,
-      brokers: KAFKA_HOST.split(',')
-    });
+module.exports.handler = async (event, context) => {
+  try {
+    const message = JSON.stringify(event);
+
+    if (!kafkaProducer) {
+      kafkaProducer = await createKafkaProducer({ kafkaHost: KAFKA_HOST });
+    }
+
+    const mskResponse = await sendMessageToMSK({ kafkaProducer, topic: ADH_TOPIC, messages: [message] });
+    console.log(JSON.stringify({ sendMessageResponse: mskResponse }));
+
+  } catch (e) {
+    console.log('ERROR during execution');
+    console.log(JSON.stringify(e));
+
+    throw e;
   }
 
-  const message = { value: JSON.stringify(event) };
-  console.log(message);
-
-  const mskResponse = await sendMessageToMSK(kafkaProducer, MY_TOPIC, message);
-  console.log(mskResponse);
-  
   return {
     statusCode: 200,
     body: "Success"
--- kafka-msk/resources/lambda/functions/msk-producer/msk-producer.js
+++ kafka-msk/resources/lambda/functions/msk-producer/msk-producer.js
@@ -1,24 +1,92 @@
-const { Kafka } = require('kafkajs');
+const sleep = require('util').promisify(setTimeout)
+const kafka = require('kafka-node');
+const Producer = kafka.Producer;
 
-const sendMessageToMSK = async (producer, topic, messages) => {
-  const messagesToSend = Array.isArray(messages) ? messages : [messages];
+const SEND_MAX_RETRIES = 10;
 
-  return await producer.send({ topic, messages: messagesToSend });
+const createKafkaProducer = (options) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const client = new kafka.KafkaClient(options);
+
+      client.once('ready', function () {
+        console.log('client ready');
+      });
+
+      client.on('error', function (err) {
+        console.log('error on kafka client');
+        console.log(JSON.stringify(err));
+
+        reject(err);
+      })
+
+      client.on('socket_error', function (err) {
+        console.log('error on kafka client (socket_error)');
+        console.log(JSON.stringify(err));
+
+        reject(err);
+      })
+      
+      const producer = new Producer(client);
+
+      producer.once('ready', function () {
+        resolve(producer);
+      });
+
+      producer.on('error', function (err) {
+        console.log('error on producer connection');
+        console.log(JSON.stringify(err));
+
+        reject(err);
+      })
+    } catch (e) {
+      console.log('error on creating kafka producer');
+      reject(e);
+    }
+  })
 }
 
-const createKafkaProducer = async ({ clientId, brokers }) => {
-  const kafka = new Kafka({
-    clientId,
-    brokers
-  });
+const sendMessage = (producer, topic, messages) => {
+  return new Promise((resolve, reject) => {
+    const messagesToSend = Array.isArray(messages) ? messages : [messages];
 
-  const producer = kafka.producer();
-  await producer.connect();
-  
-  return producer;
+    const payloads = [
+      { topic, messages: messagesToSend }
+    ];
+
+    producer.send(payloads, function (err, data) {
+      if (err) {
+        console.log('error sending the message', JSON.stringify(data));
+        reject(err);
+      }
+      resolve(data);
+    });
+  })
+}
+
+const sendMessageToMSK = async ({ kafkaProducer, topic, messages, maxRetry = SEND_MAX_RETRIES }) => {
+  let retry = 0;
+
+  while (maxRetry > retry) {
+    try {
+      const mskResponse = await sendMessage(kafkaProducer, topic, messages);
+
+      return mskResponse;
+    } catch (e) {
+      retry++;
+
+      await sleep(1000 * (retry / 2));
+
+      if (maxRetry == retry) {
+        throw e;
+      }
+
+      console.log(`Retrying to send the message, attempt: ${retry}`);
+    }
+  }
 }
 
 module.exports = {
-  createKafkaProducer,
-  sendMessageToMSK
+  sendMessageToMSK,
+  createKafkaProducer
 }
