14fae4c0042a624c49629f4a8dcf6ba592be8dc6
Almir Zulic <almir@bind.ba>
2021-07-18 23:32:15 +0200

    rename csharp sampe plus sam template

--- sls-gradual-deployments-csharp/functions/Before.cs
+++ sls-gradual-deployments-csharp/functions/Before.cs
@@ -2,20 +2,27 @@ using System.Threading.Tasks;
 using Amazon.CodeDeploy;
 using Amazon.CodeDeploy.Model;
 using System;
+using Amazon.Lambda.Core;
 
 namespace AwsDotnetCsharp
 {
 
-    public class After
+    public class Before
     {
-       public async Task<string> Handler(CodeDeployRequest request)
+      [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.CamelCaseLambdaJsonSerializer))]
+      public async Task<string> Handler(CodeDeployRequest request)
        {
+
+          Console.WriteLine(request.DeploymentId);
+
+
           AmazonCodeDeployClient	client = new AmazonCodeDeployClient();
           PutLifecycleEventHookExecutionStatusRequest req = new PutLifecycleEventHookExecutionStatusRequest();
 
           req.DeploymentId = request.DeploymentId;
           req.LifecycleEventHookExecutionId = request.LifecycleEventHookExecutionId;
-          // SET STATUS TO Falied or Succed based on CUSTOM LOGIC
+
+          // SET STATUS TO Failed or Succeeded based on CUSTOM LOGIC
           req.Status = "Succeeded";
           await client.PutLifecycleEventHookExecutionStatusAsync(req);
           return "Go Serverless v1.0! Your function executed successfully!";
