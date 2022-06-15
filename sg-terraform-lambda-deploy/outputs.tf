6d745aaa4c784caf74c2e34c989e56b9b4a88cc6
Jaymit Bhoraniya <jaymit.bhoraniya@gmail.com>
2022-06-15 17:36:14 +0530

    sg-terraform-lambda-deploy - Create And Deploy Lambda Using Terraform

---
+++ sg-terraform-lambda-deploy/outputs.tf
@@ -0,0 +1,13 @@
+# Output value definitions
+
+output "lambda_bucket_name" {
+  description = "Name of the S3 bucket used to store function code."
+
+  value = aws_s3_bucket.lambda_bucket.id
+}
+
+output "function_name" {
+  description = "Name of the Lambda function."
+
+  value = aws_lambda_function.sg_demo.function_name
+}
