e6706db1a2676e471e0522b17257150ac910673a
Almir Zulic <almir@bind.ba>
2025-01-11 12:14:17 +0100

    update tests

--- tf-azure-aws-secrets-push-replication/test/create-secrets.js
+++ tf-azure-aws-secrets-push-replication/test/create-secrets.js
@@ -1,17 +1,18 @@
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
 
-const keyVaultName = "your-keyvault-name";
-const url = `https://${keyVaultName}.vault.azure.net`;
-const client = new SecretClient(url, new DefaultAzureCredential());
+const client = new SecretClient('https://secrets-azure-aws-kv.vault.azure.net', new DefaultAzureCredential());
 
 const createSecrets = async () => {
+  const secrets = [];
   for (let i = 1; i <= 5; i++) {
     const secretName = `test-secret-${i}`;
     const secretValue = `random-value-${Math.random().toString(36).substring(2)}`;
-    await client.setSecret(secretName, secretValue);
-    console.log(`Created secret: ${secretName}`);
+    secrets.push(client.setSecret(secretName, secretValue));
   }
+  await Promise.all(secrets);
 };
 
-createSecrets().catch(console.error);
+createSecrets()
+  .then(() => console.log('Secrets created successfully'))
+  .catch(console.error);
